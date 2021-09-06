11c64a0a677971fa3bcfe4c37bdbbacd63e6dcd3
Sharm-Dev <sharmainetaylor@ymail.com>
2021-09-06 18:57:00 +0100

    Getting a console.log of shows

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
+  for(episodes in allEpisodes){
+    console.log(allEpisodes[episodes].name);
+  }
   makePageForEpisodes(allEpisodes);
 }
 
@@ -8,5 +11,11 @@ function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
+
+
+
+
+
+
 
 window.onload = setup;
