3221ee5bc13d280a0c3863fce968ee95c6fa3e74
Sharm-Dev <sharmainetaylor@ymail.com>
2021-09-14 20:51:00 +0100

    Code update - search and dropdown added

--- script.js
+++ script.js
@@ -1,66 +1,105 @@
 const contentEl = document.getElementById('content');
+const headerEl = document.getElementById("headerBar");
+const allEpisodes = getAllEpisodes();
 
 
-function displayEpisodes(searchTerm) {
+/*----Function that is the page set up----*/
+function setup() {
   contentEl.innerHTML = '';
-  const allEpisodes = getAllEpisodes();
-  
-
-  const searchEsp = allEpisodes.filter(episode => {
-    return (episode.name).toLowerCase().includes(searchTerm.toLowerCase()) || (episode.summary).toLowerCase().includes(searchTerm.toLowerCase());
-  })
+  makePageCardForEpisodes(allEpisodes);
+  // dropdownMenu(allEpisodes);
+  dropdownMenu(allEpisodes);
+}
 
 
-  for(numberOfLoop in searchEsp){
-
-    const movieImg = searchEsp[numberOfLoop].image.medium;
-    const movieTitle = searchEsp[numberOfLoop].name;
-    const movieSummary = searchEsp[numberOfLoop].summary;
-    const movieSeasonNumber = searchEsp[numberOfLoop].season;
-    let movieEpisodesNumber = searchEsp[numberOfLoop].number;
-
-    const movieCardUrl = searchEsp[numberOfLoop].url;
-  
+/*--------------------------------------------------------------------------------*/
 
 
+/*----Function in with a Loop in for making all the cards----*/
+function makePageCardForEpisodes(episodeList){
+
+  contentEl.innerHTML = "";
+
+  episodeList.forEach((createEpisodeCard) => {
+    const movieEl = document.createElement('main');
+    const movieUrl = createEpisodeCard.url;
+    const movieImg = createEpisodeCard.image.medium;
+    const movieTitle = createEpisodeCard.name;
+    const movieSummary = createEpisodeCard.summary;
+    const movieSeasonNumber = createEpisodeCard.season;
+    let movieEpisodesNumber = createEpisodeCard.number;
     if(movieEpisodesNumber < 10){
       movieEpisodesNumber = `0${movieEpisodesNumber}`
     }
     const episodeCode = `S0${movieSeasonNumber}E${movieEpisodesNumber}`;
-
-
-    const movieEl = document.createElement('div');
+    
     movieEl.innerHTML=
     `<div id="movieCard">
-      <h1>${movieTitle} - ${episodeCode}</h1> 
+    <button><a href="${movieUrl}" target="_blank">
+      <h2>${movieTitle} - ${episodeCode}</h2> 
+    </a></button>
       <img
             src="${movieImg}"
             alt="${movieTitle}"
       />
 
-        <p>${movieSummary}</p>
+        <div id="cardSummary">${movieSummary}</div>
     </div>`;
-
-
-
     contentEl.appendChild(movieEl);
+  });
+  showEpisodes(episodeList)
+}
 
-  }
+/*---------------------------------Dropdown Menu----------------------------------*/
 
+
+/*----Function with Map Function for Search bar----*/ 
+function dropdownMenu(episodeList){
+  const selectEl = document.createElement('select');
+  const options = episodeList.map(movie => `<option value="${movie.name}">${movie.name}-${movie.name}</option>`);
+  selectEl.innerHTML = options.join('');
+  console.log(selectEl);
+  document.getElementById('root').appendChild(selectEl);
+}
+
+/*-----------------------------------------Search Bar------------------------------*/
+
+/*----Event listener for Function Search bar----*/
+  const searchEl = document.getElementById('search');
+  form.addEventListener("input", (e) => {
+  e.preventDefault();
+  const searchTerm = searchEl.value;
+  console.log("search Episodes:", searchTerm);
+  filterSearchResults(allEpisodes, searchTerm.toLowerCase());
+});
+
+/*----Fliter Function for Search bar----*/
+function filterSearchResults(arrayOfEpisodes, searchInput) {
+    filteredEpisodes = arrayOfEpisodes.filter((episode) => {
+    return (
+      episode.summary.toLowerCase().includes(searchInput) ||
+      episode.name.toLowerCase().includes(searchInput)
+    );
+  })
+  makePageCardForEpisodes(filteredEpisodes)
+   console.log(filteredEpisodes)
+}
 
   
-  makePageForEpisodes(allEpisodes);
-}
-const searchEl = document.getElementById('search');
-searchEl.addEventListener('input',(event) =>{
-  const searchTerm = event.target.value
-  displayEpisodes(searchTerm);
-})
+/*--------------------------------------------------------------------------------*/
 
 
 
-function makePageForEpisodes(episodeList) {
+/*----Function that displays how many epsisodes are displaying on the webpage----*/  
+function showEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
-window.onload = displayEpisodes;
+
+/*--------------------------------------------------------------------------------*/
+
+window.onload = setup;
+
+
+
+
